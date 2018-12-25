1326bc17be9e1ef2fe7705bab18a726abf68d986
kaier <kaier0121@163.com>
2018-12-25 12:54:06 +0800

    qqlogin

--- app/controller/callback.js
+++ app/controller/callback.js
@@ -5,17 +5,13 @@ const client_secret = '3c4a6dfdde0060a61d2c79f620b2553c'
 
 const Controller = require('egg').Controller;
   class Callback extends Controller {
-    // async qqLogin() {
-    //   const callBackUrl = 'https://www.kaier001.com/api/v1/callback/qq'
-    //   const step1Url = `https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=${client_id}&redirect_uri=${callBackUrl}&state=qq&scope=get_user_info`
-    //   // Step1：获取Authorization Code
-    //   // window.open(step1Url)
-    //   await axios.get(step1Url).then(res=>{
-    //     console.log('step1返回值',res);
-    //     // code = res.data.code
-    //     // state = res.data.state
-    //   }) 
-    // }
+    async qqLogin() {
+      const { ctx } = this
+      // Step1：获取Authorization Code
+      const callBackUrl = 'https://www.kaier001.com/api/v1/callback/qq'
+      const step1Url = `https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=${client_id}&redirect_uri=${callBackUrl}&state=qq&scope=get_user_info`
+      ctx.redirect(step1Url)
+    }
 
     async qq() {
       const { ctx } = this
@@ -25,7 +21,7 @@ const Controller = require('egg').Controller;
       const step2Url = `https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&client_id=${client_id}&client_secret=${client_secret}&code=${code}&redirect_uri=${callBackUrl}`
       let result;
       await axios.get(step2Url).then(res=>{
-        result = res.data
+        result = res
       }) 
       ctx.body = {
         msg:'success',
