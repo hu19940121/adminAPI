a49362ecf079b2c052df31653c9e54f3f28a0c91
kaier <kaier0121@163.com>
2018-12-25 14:54:50 +0800

    qqlogin

--- app/controller/callback.js
+++ app/controller/callback.js
@@ -12,23 +12,41 @@ const Controller = require('egg').Controller;
       const step1Url = `https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=${client_id}&redirect_uri=${callBackUrl}&state=qq&scope=get_user_info`
       ctx.redirect(step1Url)
     }
-
     async qq() {
       const { ctx } = this
       const code = ctx.query.code
-      const state = ctx.query.state
+      const callBackUrl = 'https://www.kaier001.com/api/v1/callback/qq'
       //Step2：通过Authorization Code获取Access Token
       const step2Url = `https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&client_id=${client_id}&client_secret=${client_secret}&code=${code}&redirect_uri=${callBackUrl}`
-      let result;
+      let accessTokenObj = {};
+      let openIdObj = {}
+      let userInfo = {}
       await axios.get(step2Url).then(res=>{
-        result = res
-      }) 
+        let accessTokenStr = res.data //获取到accessToken字符串
+        accessTokenStr.split('&').map(item=>{
+          console.log("item.split('=')[0].toString()",item.split('=')[0].toString());
+          console.log("item.split('=')[0]",item.split('=')[0]);
+          let key = item.split('=')[0].toString()
+          console.log("key",key);
+          console.log("typeofkey",typeof(key));
+          accessTokenObj[key] = item.split('=')[1]
+        })
+      })  
+      // Step3：通过Access Token Code获取openId
+      const step3Url = `https://graph.qq.com/oauth2.0/me?access_token=${accessTokenObj.access_token}`
+      await axios.get(step3Url).then(res=>{
+        openIdObj = JSON.parse(res.data.replace(/[\r\n]/g,"").match(/^callback\((.*)\);$/)[1]); //callback( {"error":100020,"error_description":"code is reused error"} ) 转为obj
+      })
+      // Step4: 获取用户信息
+      const getUserInfoUrl = `https://graph.qq.com/user/get_user_info?access_token=${accessTokenObj.access_token}&oauth_consumer_key=${client_id}&openid=${openIdObj.openid}`
+      await axios.get(getUserInfoUrl).then(res=>{
+        userInfo = res.data
+      })
       ctx.body = {
         msg:'success',
         code:0,
-        data:result
+        userInfo
       }
-      
     }
   }
 module.exports = Callback
